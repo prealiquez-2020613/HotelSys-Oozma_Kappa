aaba7584f33650971fba08fbb74834ae782c7fc4
P.Realiquez <prealiquez-2020613@kinal.edu.gt>
2025-05-01 17:37:57 -0600

    Se define las acciones permitidas para los administradores de hotel

--- src/hotel/hotel.routes.js
+++ src/hotel/hotel.routes.js
@@ -7,15 +7,15 @@ import {
     getHotel
 } from './hotel.controller.js'
 
-import { validateJwt, adminValidation } from '../../middlewares/validate.jwt.js'
+import { validateJwt, adminValidation, hotelAdminValidation } from '../../middlewares/validate.jwt.js'
 import { hotelValidator, updatehotelValidator } from '../../helpers/validators.js'
 
 const api = Router();
 
 // RUTAS PRIVADAS 
 api.post('/createHotel', [validateJwt, adminValidation, hotelValidator], saveHotel)
-api.put('/updateHotel/:id', [validateJwt], [adminValidation], [updatehotelValidator], updateHotel)
-api.put('/deleteHotel/:id', [validateJwt], [adminValidation],  deleteHotel)
+api.put('/updateHotel/:id', [validateJwt], [hotelAdminValidation], [updatehotelValidator], updateHotel)
+api.put('/deleteHotel/:id', [validateJwt], [hotelAdminValidation],  deleteHotel)
 api.get('/getAllHotels', [validateJwt], getAllHotels)
 api.get('/getHotel/:id', [validateJwt], getHotel)
 
--- src/service/service.routes.js
+++ src/service/service.routes.js
@@ -1,15 +1,15 @@
 import { Router } from 'express';
 import {saveService,updateService,deleteService,getAllServices,getService,getServicesByHotel} from './service.controller.js'
 
-import { validateJwt, adminValidation } from '../../middlewares/validate.jwt.js'
+import { validateJwt, adminValidation, hotelAdminValidation } from '../../middlewares/validate.jwt.js'
 import { saveServiceValidator, updateServiceValidator, deleteServiceValidator, getServiceValidator, getServicesByHotelValidator} from '../../helpers/validators.js'
 
 const api = Router()
 
 // Rutas con validaciones y middlewares de autenticación
-api.post('/createService', [validateJwt, adminValidation, saveServiceValidator], saveService)
-api.put('/updateService/:id', [validateJwt, adminValidation, updateServiceValidator], updateService)
-api.delete('/deleteService/:id', [validateJwt, adminValidation, deleteServiceValidator], deleteService)
+api.post('/createService', [validateJwt, hotelAdminValidation, saveServiceValidator], saveService)
+api.put('/updateService/:id', [validateJwt, hotelAdminValidation, updateServiceValidator], updateService)
+api.delete('/deleteService/:id', [validateJwt, hotelAdminValidation, deleteServiceValidator], deleteService)
 api.get('/getAllServices', [validateJwt], getAllServices)
 api.get('/getService/:id', [validateJwt], getServiceValidator, getService)
 api.get('/getServicesByHotel/:hotelId', [validateJwt], getServicesByHotelValidator, getServicesByHotel)
